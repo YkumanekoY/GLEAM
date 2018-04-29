c894a27c97b893bc073273d51ab0608a4196b317
Yua Akimoto <>
2018-04-30 08:11:21 +0900

    no message

--- Assets/Script/MapCreat.cs
+++ Assets/Script/MapCreat.cs
@@ -44,36 +44,6 @@ public class MapCreat : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-		if (OnTouchDown ()) {
-			Debug.Log ("タップされました");//タップ判定
-		}
-
-	}
-
-	bool OnTouchDown(){
-
-		if (0 < Input.touchCount) {
-
-			for (int i = 0; i < Input.touchCount; i++) {
-				
-				Touch t = Input.GetTouch (i);
-
-				if (t.phase == TouchPhase.Began) {
-					Ray ray = Camera.main.ScreenPointToRay (t.position);
-					RaycastHit hit = new RaycastHit ();
-					if (Physics.Raycast (ray, out hit)) {
-
-						if (hit.collider.gameObject == this.gameObject) {
-							return true;
-						}
-					}
-
-				}
-
-			}
-		}	//タップ判定
-		return false;
 	}
 
 }
