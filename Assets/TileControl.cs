5360fd739eaab923ea75769c73da042d01358809
Yua Akimoto <>
2018-04-29 17:59:46 +0900

    no message

---
+++ Assets/TileControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (OnTouchDown ()) {
+			Debug.Log ("タップされました");
+		}
+
+	}
+
+	bool OnTouchDown(){
+
+		if (0 < Input.touchCount) {
+
+			for (int i = 0; i < Input.touchCount; i++) {
+
+				Touch t = Input.GetTouch (i);
+
+				if (t.phase == TouchPhase.Began) {
+					Ray ray = Camera.main.ScreenPointToRay (t.position);
+					RaycastHit hit = new RaycastHit ();
+					if (Physics.Raycast (ray, out hit)) {
+
+						if (hit.collider.gameObject == this.gameObject) {
+							return true;
+						}
+					}
+
+				}
+
+			}
+		}
+		return false;
+	}
+}
