cdcb67efaafa98f385fcef9cd98de9b05cd77fd7
Yua Akimoto <>
2018-05-02 21:07:53 +0900

    配列でマップ生成

--- Assets/Script/MapCreat.cs
+++ Assets/Script/MapCreat.cs
@@ -6,39 +6,47 @@ public class MapCreat : MonoBehaviour {
 
 	public GameObject cube;
 	public GameObject town;
+	public GameObject wallBlock;
 
 	public int input_tate=9;	//マップ縦
 	public int input_yoko=9;	//マップ横	フィールドで入れられたらいいね
 
-	float xPos=0; //スタート位置
-	float zPos=0; //スタート位置
+	//float xPos=0; //スタート位置
+	//float zPos=0; //スタート位置
 
-	int[,] array_MapList = new int[9,9];	//マップ配列
+	//ステージ配列　0:床, 1:壁
+	public int[,] stageArray = new int[11,11]{
+		{1,1,1,1,1,1,1,1,1,1,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,1,1,1,1,1,1,1,1,1,1}
+	};	
+
 
 	// Use this for initialization
 	void Start () {
 		
-		for (int i = 0; i<input_yoko; i++) {
-			for (int j=0; j<input_tate; j++) {
-				
-				if (i == (input_tate - 1) / 2 && j == (input_yoko - 1) / 2) {
-					GameObject.Instantiate (town, new Vector3 (xPos, 0, zPos), new Quaternion ());
-					array_MapList [(input_tate - 1) / 2, (input_yoko - 1) / 2] = 4;
-				}
-				else {
-					GameObject.Instantiate (cube, new Vector3 (xPos, 0, zPos), new Quaternion ());
-				}
+		for (int i = 0; i < stageArray.GetLength(0); i++) {
+			for (int j = 0; j < stageArray.GetLength(1); j++) {
+				if (stageArray[i,j] == 0) {
+					if (i == 5 && j == 5) {
+						Instantiate (town, new Vector3 (i, -1, j), Quaternion.identity);
+					} else {
+						Instantiate(cube, new Vector3(i, -1, j), Quaternion.identity);
+					}
+				} else {
+					Instantiate(wallBlock, new Vector3(i, 0, j), Quaternion.identity);
 
-				xPos += cube.transform.localScale.x;
-				//array_MapList [i, j] = 0;
-
+				}
 			}
-			xPos = 0;
-			zPos += cube.transform.localScale.z;
 		}
-
-
-		Debug.Log (array_MapList);
 
 	}
 	
