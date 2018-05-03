dfa7f8c4b2005b718781453482380646e2006f20
Yua Akimoto <>
2018-05-03 10:49:28 +0900

    マップ調整

--- Assets/Script/MapCreat.cs
+++ Assets/Script/MapCreat.cs
@@ -7,9 +7,10 @@ public class MapCreat : MonoBehaviour {
 	public GameObject cube;
 	public GameObject town;
 	public GameObject wallBlock;
+	public GameObject Lump;
 
-	public int input_tate=9;	//マップ縦
-	public int input_yoko=9;	//マップ横	フィールドで入れられたらいいね
+	//int input_tate=9;	//マップ縦
+	//int input_yoko=9;	//マップ横	フィールドで入れられたらいいね
 
 	//float xPos=0; //スタート位置
 	//float zPos=0; //スタート位置
@@ -19,10 +20,10 @@ public class MapCreat : MonoBehaviour {
 		{1,1,1,1,1,1,1,1,1,1,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,2,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,2,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
@@ -38,10 +39,14 @@ public class MapCreat : MonoBehaviour {
 				if (stageArray[i,j] == 0) {
 					if (i == 5 && j == 5) {
 						Instantiate (town, new Vector3 (i, -1, j), Quaternion.identity);
-					} else {
+						stageArray [i, j] = 4;
+					} 
+					else {
 						Instantiate(cube, new Vector3(i, -1, j), Quaternion.identity);
 					}
-				} else {
+				}else if(stageArray[i,j] == 2){
+					Instantiate(Lump, new Vector3(i, -1, j), Quaternion.identity);
+				}else {
 					Instantiate(wallBlock, new Vector3(i, 0, j), Quaternion.identity);
 
 				}
